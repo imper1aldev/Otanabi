35bfca362ae5d29b5e2f62f4048b4a25d01790ab
Havsalazar <havsa@DESKTOP-9C5TO09>
2024-07-30 21:34:19 -0500

    primitive version of versionchecker

---
+++ AnimeWatcher.Core/Services/AppUpdateService.cs
@@ -0,0 +1,13 @@
+﻿namespace AnimeWatcher.Core.Services;
+public class AppUpdateService
+{
+    private readonly HttpService _http = new();
+    private readonly string githubUrl = "https://raw.githubusercontent.com/havsalazar/AnimeWatcher/master/AnimeWatcher/version.v";
+
+    public async Task<String> CheckGitHubVersion()
+    {
+        var response = await _http.GetAsync(githubUrl);
+        return response;
+    }
+
+}
