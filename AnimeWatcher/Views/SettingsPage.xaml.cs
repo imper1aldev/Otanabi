ab9bddf48afd40230084327670e4ccc5f7277d96
Havsalazar <havsa@DESKTOP-9C5TO09>
2024-08-04 23:43:25 -0500

    remove typos

--- AnimeWatcher/Views/SettingsPage.xaml.cs
+++ AnimeWatcher/Views/SettingsPage.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using AnimeWatcher.ViewModels;
-
 using Microsoft.UI.Xaml.Controls;
 
 namespace AnimeWatcher.Views;
 
-// TODO: Set the URL for your privacy policy by updating SettingsPage_PrivacyTermsLink.NavigateUri in Resources.resw.
 public sealed partial class SettingsPage : Page
 {
     public SettingsViewModel ViewModel
